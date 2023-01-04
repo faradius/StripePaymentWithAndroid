628e6f2b5bd65f276b4cfbd82dbaed5c1c861060
Alejandro <faradius.magic187@gmail.com>
2023-01-03 18:48:08 -0600

    Adapatando el codigo para  carrito de compras (proyectos futuros)

--- server.js
+++ server.js
@@ -12,16 +12,18 @@ const calculateOrderAmount = (items) => {
   // Replace this constant with a calculation of the order's amount
   // Calculate the order total on the server to prevent
   // people from directly manipulating the amount on the client
-  return 100;
+  console.log(items[0].amount)
+  return items[0].amount;
 };
 
 app.post("/create-payment-intent", async (req, res) => {
   const { items } = req.body;
+  const { currency } = req.body;
 
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
     amount: calculateOrderAmount(items),
-    currency: "usd",
+    currency: currency,
     //automatic_payment_methods: {
      // enabled: true,
     //},
